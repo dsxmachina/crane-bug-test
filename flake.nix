905096924db7285bc04223c56827f820fd0d3735
Christian Hammermeister <christian.hammermeister@iml.fraunhofer.de>
2024-05-21 16:31:13 +0200

    basic example

---
+++ flake.nix
@@ -0,0 +1,132 @@
+{
+  description = "Build a cargo workspace";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          buildInputs = [
+            # Add additional build inputs here
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        };
+
+        craneLibLLvmTools = craneLib.overrideToolchain
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]);
+
+        # Build *just* the cargo dependencies (of the entire workspace),
+        # so we can reuse all of that work (e.g. via cachix) when running in CI
+        # It is *highly* recommended to use something like cargo-hakari to avoid
+        # cache misses when building individual top-level-crates
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        individualCrateArgs = commonArgs // {
+          inherit cargoArtifacts;
+          inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
+          # NB: we disable tests since we'll run them all via cargo-nextest
+          doCheck = false;
+        };
+
+        fileSetForCrate = crate: lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            ./dummy
+            ./proc_macro
+            crate
+          ];
+        };
+
+        # Build the top-level crates of the workspace as individual derivations.
+        # This allows consumers to only depend on (and build) only what they need.
+        # Though it is possible to build the entire workspace as a single derivation,
+        # so this is left up to you on how to organize things
+        dummy = craneLib.buildPackage (individualCrateArgs // {
+          pname = "dummy";
+          cargoExtraArgs = "-p dummy";
+          src = fileSetForCrate ./dummy;
+        });
+        proc_macro = craneLib.buildPackage (individualCrateArgs // {
+          pname = "proc_macro";
+          cargoExtraArgs = "-p proc_macro";
+          src = fileSetForCrate ./proc_macro;
+        });
+      in
+      {
+        checks = {
+          # Build the crates as part of `nix flake check` for convenience
+          inherit dummy proc_macro;
+        };
+
+        packages = {
+          inherit dummy proc_macro;
+        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          my-workspace-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
+            inherit cargoArtifacts;
+          });
+        };
+
+        apps = {
+          dummy = flake-utils.lib.mkApp {
+            drv = dummy;
+          };
+          proc_macro = flake-utils.lib.mkApp {
+            drv = proc_macro;
+          };
+        };
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [ ];
+        };
+      });
+}
---
+++ proc_macro/src/lib.rs
@@ -0,0 +1,6 @@
+use proc_macro::{self, TokenStream};
+
+#[proc_macro_derive(Command)]
+pub fn command_derive(_input: TokenStream) -> TokenStream {
+    todo!()
+}
